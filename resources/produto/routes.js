971d8f53fcb23556af0b244fc97d6d5abb65acab
mrcsporto <marilia.mota90@gmail.com>
2023-04-03 12:43:21 -0300

    verbos http criados

--- resources/produto/routes.js
+++ resources/produto/routes.js
@@ -22,13 +22,44 @@ app.post(BASE_URL, async (req, res) => {
     let corpo = req.body;
 
     let sql = await database.execute(`
-        INSERT INTO tb_product (titulo, descricao, imagem)
-        VALUES ('${corpo.titulo}', '${corpo.descricao}', '${corpo.imagem}')
+        INSERT INTO tb_product (nome, descricao, img_id, valor, tamanho, cor, quantidade, referencia, marca_id, estado, colecoes_id, rewies, desconto, categoria_id)
+        VALUES ('${corpo.nome}', '${corpo.descricao}', '${corpo.img_id}', '${corpo.valor}', '${corpo.tamanho}', '${corpo.cor}', '${corpo.quantidade}', '${corpo.referencia}', '${corpo.marca_id}', '${corpo.estado}', '${corpo.colecoes_id}', '${corpo.rewies}', '${corpo.desconto}', '${corpo.categoria_id}')
     `);
 
     corpo.id = sql.insertId;
 
     res.send(corpo);
-})
+});
+
+app.patch(`${BASE_URL}:id`, async (req, res) => {
+    let dados = req.body;
+
+    let jaExiste = await database.execute(`
+        SELECT * FROM tb_product WHERE id='${req.params.id}'
+    `);
+
+    if (undefined === jaExiste[0]) {
+        res.sendStatus(404);
+        return;
+    }
+
+    await database.execute(`
+        UPDATE tb_product SET 
+            titulo='${req.body.titulo || jaExiste[0].titulo}',
+            descricao='${req.body.descricao || jaExiste[0].descricao}',
+            imagem='${req.body.imagem || jaExiste[0].imagem}'
+        WHERE id='${req.params.id}'
+    `);
+
+    dados.id = req.params.id;
+
+    res.send(dados);
+});
+
+app.delete(`${BASE_URL}/:id`, async (req, res) => {
+    await database.execute(`DELETE FROM tb_product WHERE id='${req.params.id}'`)
+
+    res.sendStatus(204);
+});
 
 module.exports = app;
